1f7990f5e8d6f36a601b77cf1869a164b6c299ea
Chris Poirier <chris@courage-my-friend.org>
2011-08-08 15:31:31 -0400

    Removed some cruft before moving on.

--- lib/schemaform/adapters/generic/mapping/entity_map.rb
+++ lib/schemaform/adapters/generic/mapping/entity_map.rb
@@ -35,23 +35,6 @@ class EntityMap
       @links        = {}  
       @attribute_mappings = {}
       @schema_map.register_table(anchor_table)
-      
-      
-      
-      
-      # @ancestors    = base_map ? @base_map.ancestors + [@base_map] : []
-
-      # @links = {}  parent_table => [child_tables]
-      #              parent_table => { child_table => link info }
-      #              
-      #        OR    child_table => { parent_table => link info }
-      #        
-      # ALSO
-      # if you add a link to an identifier that is itself a reference, you should add a synonym link that skips the intermediary.
-      #    
-      # You are building a DAG. Don't you already have one of those written?
-      # 
-      # table => 
    end
 
    attr_reader :schema_map, :entity, :anchor_table, :base_map
@@ -76,66 +59,6 @@ class EntityMap
       @attribute_mappings[attribute] = [] unless @attribute_mappings.member?(attribute)
       @attribute_mappings[attribute] << field
    end
-
-
-
-#    
-#    def add_table( table, parent_table, link_field, table.identifier, entity_map.ancestors.count )
-#       
-#    end
-#    
-#    
-#    
-#    
-#    
-#    
-#    
-#    def capture_tuple_attribute( attribute )
-#       capture_attribute(attribute) do
-#          @attribute_mappings[attribute] = []
-#          yield
-#       end
-#    end
-#    
-#    alias capture_scalar_attribute capture_attribute
-#    alias capture_set_attribute    capture_attribute
-#    alias capture_list_attribute   capture_attribute
-#    
-#    def define_field( field, purpose = nil )
-#       @capturing.each do |context|
-#          context << attribute if context.is_an?(Array)
-#       end
-#       
-#       #
-#       # Given a new ParentLink, calculates all potential JoinPaths that can be used to 
-#       # assemble a subset of the originating Entity.
-# 
-#       def add_path( field, structural_link )
-#          @parent = structural_link.table if structural_link.direct_parent?
-#          @parent.paths.each do |parent_path|
-#             @paths << parent_path + 
-#          end
-#       end
-# 
-# 
-#       
-#       WORKING HERE -- FIGURE OUT IF WE WOULD STORE EVERY FIELD IN ALL CONTEXTS
-#       
-#       if @attribute_mappings.member?(@capturing)
-#    end
-#    
-# protected
-# 
-#    def capture_attribute( attribute )
-#       @capturing.push(attribute)
-#       begin      
-#          @attribute_mappings[attribute] = {}
-#          yield
-#       ensure
-#          @capturing.pop()
-#       end
-#    end
-# 
 
 end # EntityMap
 end # Generic
